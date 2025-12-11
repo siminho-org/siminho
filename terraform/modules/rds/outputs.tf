dbc13cc3ee5f518bfd406e5da36fa3120de3bf24
sbreuer <simon.breuer@inform-software.com>
2025-12-11 17:24:47 +0100

    add terraform code

---
+++ terraform/modules/rds/outputs.tf
@@ -0,0 +1,40 @@
+output "db_instance_id" {
+  description = "RDS instance ID"
+  value       = aws_db_instance.main.id
+}
+
+output "db_endpoint" {
+  description = "RDS instance endpoint"
+  value       = aws_db_instance.main.endpoint
+}
+
+output "db_address" {
+  description = "RDS instance address"
+  value       = aws_db_instance.main.address
+}
+
+output "db_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.main.port
+}
+
+output "db_name" {
+  description = "Database name"
+  value       = aws_db_instance.main.db_name
+}
+
+output "db_username" {
+  description = "Database master username"
+  value       = var.db_username
+  sensitive   = true
+}
+
+output "db_security_group_id" {
+  description = "Security group ID for RDS"
+  value       = aws_security_group.rds.id
+}
+
+output "db_secret_arn" {
+  description = "ARN of the Secrets Manager secret containing DB credentials"
+  value       = aws_secretsmanager_secret.db_password.arn
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,101 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "siminho"
+}
+
+# VPC Configuration
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use single NAT Gateway instead of one per AZ (cost savings)"
+  type        = bool
+  default     = true
+}
+
+# EKS Configuration
+variable "eks_cluster_version" {
+  description = "Kubernetes version for EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "eks_node_groups" {
+  description = "EKS node group configurations"
+  type = map(object({
+    desired_size   = number
+    min_size       = number
+    max_size       = number
+    instance_types = list(string)
+    capacity_type  = string
+  }))
+  default = {
+    general = {
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 4
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+}
+
+# RDS Configuration
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "siminho"
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+  default     = "postgres"
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "Allocated storage for RDS in GB"
+  type        = number
+  default     = 20
+}
